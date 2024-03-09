b58edea14e978169eb046891a1413d7967258263
vincenzoaltavilla <vincenzoaltavilla.va@gmail.com>
2024-03-09 11:21:11 +0100

    first commit: getting new url

--- get_url_SC.py
+++ get_url_SC.py
@@ -6,10 +6,12 @@ def get_url_SC():
     URL_NEW_LINK = "https://tecnologiacasa.it/streamingcommunity-nuovo-indirizzo/"
     PREFIX_SC = "https://streamingcommunity."
 
+    # HTTP get request and text extraction
     response = requests.get(URL_NEW_LINK)
     response.raise_for_status()
     soup = bs4.BeautifulSoup(response.text, 'html.parser')
 
+    # find div with new SC link and extract every link
     div_with_new_link = soup.find('div', class_='entry-content')
     every_link_in_div = div_with_new_link.find_all('a')
 
