45c0477e3769c50086a5e1d3069f4278f908027e
vince <vincenzoaltavilla.va@gmail.com>
2024-03-10 21:47:45 +0100

    Top10 gained!

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from get_url_SC import *
-import bs4
-import requests
+from selenium.webdriver import Chrome
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
-# HTTP get request and text extraction
-url = get_url_SC()+'browse/top10/'
-response = requests.get(url)
-response.raise_for_status()
-soup = bs4.BeautifulSoup(response.text, 'html.parser')
+# Software which navigates with Chrome
+chrome_driver = ChromeDriverManager().install()
 
+# What we are going to use to give orders to Chrome
+driver = Chrome(service=Service(chrome_driver))
+driver.maximize_window()
+
+# Open browser at URL
+url = get_url_SC() + 'browse/top10/'
+driver.get(url)
+
+top10 = []
+show_covers = driver.find_elements(By.CLASS_NAME, "box-16x9")
+
+for cover_img in show_covers:
+    show = cover_img.find_element(By.TAG_NAME, "img").get_attribute("alt")
+    top10.append(show)
+
+# Close browser
+driver.quit()
+
+print(top10)
