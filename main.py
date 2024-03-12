bda5a4e19427cf4c7a9fa00987cdc42c8a2d8173
vincenzoaltavilla <vincenzoaltavilla.va@gmail.com>
2024-03-12 10:40:23 +0100

    store ONE top10 into excel

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
 from get_top10 import *
+import pandas as pd
+from openpyxl import load_workbook
 
-print(get_top10())
+top10 = get_top10()
+
+df = pd.DataFrame(top10, index=['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'], columns=["Top10"])
+
+# Specify Excel file name
+excel_file = "top10.xlsx"
+
+# Write DataFrame to Excel
+df.to_excel(excel_file, sheet_name="TOP10")
+
+wb = load_workbook(excel_file)
+ws = wb["TOP10"]
+
+alphabet = 'BCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+for letter in alphabet:
+
+    try:
+        length = len(ws[letter + '1'].value)
+    except:
+        break
+
+    max_width = 0
+
+    for row_number in range(1, ws.max_row + 1):
+        if len(ws[f'{letter}{row_number}'].value) > max_width:
+            max_width = len(ws[f'{letter}{row_number}'].value)
+
+    ws.column_dimensions[letter].width = max_width + 1
+
+wb.save(excel_file)
